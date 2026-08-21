5b6829f775652f6839df136603196371a8ceaee6
Galuh Abada <galuh_abada@teknokrat.ac.id>
2026-08-22 00:10:32 +0700

    initial commit

--- js/projects.js
+++ js/projects.js
@@ -3,20 +3,19 @@
 
 window.PROJECTS = [
   {
-    title: "IoT Wheelchair Tracker",
-    description: "Prototipe kursi roda IoT: GPS tracking + kontrol joystick (concept).",
-    tags: ["IoT", "ESP32", "GPS"],
-    image: "../assets/img/project1.jpeg",
-    live: "",
-    repo: ""
-  },
-  {
-    title: "Vigenère Cipher",
-    description: "untuk enkripsi dan dekripsi teks menggunakan algoritma Vigenère.",
-    tags: ["Web", "JavaScript", "Cipher"],
-    image: "../assets/img/project2.png",
-    live: "https://galuh1305.github.io/vigenere_chiper/ ",
-    repo: "https://github.com/Galuh1305/vigenere_chiper.git" // isi kalau ada repo github project
-  }
+  title: "Smart Weather Station",
+  description: "Mengembangkan sistem peringatan dini cuaca ekstrem bersama tim.",
+  tags: ["IoT", "ESP32", "Sensor"],
+  image: "../assets/img/project1.jpeg",
+  liveUrl: "https://lampung.tribunnews.com/pendidikan/1215771/teknologi-untuk-mitigasi-cuaca-ekstrem-mahasiswa-teknokrat-kembangkan-smart-weather-station"
+},
+{
+  title: "Vigenère Cipher",
+  description: "Untuk enkripsi dan dekripsi teks menggunakan algoritma Vigenère.",
+  tags: ["Web", "JavaScript", "Cipher"],
+  image: "../assets/img/project2.png",
+  liveUrl: "https://galuh1305.github.io/vigenere_chiper/",
+  repoUrl: "https://github.com/Galuh1305/vigenere_chiper.git"
+}
   
 ];
